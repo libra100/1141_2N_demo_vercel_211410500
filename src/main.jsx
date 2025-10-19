769e756bd00a17b319b24901ef8a2c06907f534f
libra100 <41791240+libra100@users.noreply.github.com>
2025-10-19 20:54:50 +0800

    add tailwindcss support

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './blog_00.scss';
+import './index.css';
 import App from './App_00.jsx';
 
 createRoot(document.getElementById('root')).render(
--- src/pages/HomePage_00.jsx
+++ src/pages/HomePage_00.jsx
@@ -1,8 +1,8 @@
 const HomePage_00 = () => {
   return (
-    <div>
-      <h1>HomePage_00</h1>
-      <h2>蔡松恩，211410500</h2>
+    <div className='flex flex-col justify-center items-center'>
+      <h1 className='text-2xl text-purple-700 mb-6'>HomePage_00</h1>
+      <h2 className='text-xl'>蔡松恩，211410500</h2>
     </div>
   );
 };
